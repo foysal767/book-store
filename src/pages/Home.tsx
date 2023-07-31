0cbc2b3bf33093e0a9bee49aeae269014d17ee5e
foysal767 <uddinmohammadfoysal981@gmail.com>
2023-07-31 23:39:33 +0600

    home page added without redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,18 @@ import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import Footer from "../layouts/Footer";
 import banner from "../assets/images/banner-image.jpg";
+import { useEffect, useState } from "react";
+import { IBook } from "../types/globalTypes";
+// import { IBook } from "../types/globalTypes";
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/recent-book")
+      .then((res) => res.json())
+      .then((data) => setBooks(data.data));
+  }, []);
+  console.log(books);
   return (
     <>
       <div className="w-100vw px-10">
@@ -32,32 +42,36 @@ export default function Home() {
               Top 10 Recently Added Books
             </h1>
             <div className="flex flex-wrap items-center justify-center">
-              <div className="w-full md:w-1/3 lg:w-1/4 px-4 py-4 mb-4 mx-4 my-4 overflow-hidden rounded-lg shadow-md dark:bg-gray-900 dark:text-gray-100">
-                <div className="flex space-x-4">
-                  <div className="flex flex-col space-y-1">
-                    <h2 className="mb-1 text-xl font-semibold">Title</h2>
+              {books?.map((book: IBook) => (
+                <div className="w-full md:w-1/3 lg:w-1/4 px-4 py-4 mb-4 mx-4 my-4 overflow-hidden rounded-lg shadow-md dark:bg-gray-900 dark:text-gray-100">
+                  <div className="flex space-x-4">
+                    <div className="flex flex-col space-y-1">
+                      <h2 className="mb-1 text-xl font-semibold">
+                        {book.title}
+                      </h2>
+                    </div>
                   </div>
+                  <div>
+                    <img
+                      src={book.img}
+                      alt=""
+                      className="object-cover w-full mb-4 h-60 sm:h-96 dark:bg-gray-500"
+                    />
+                    <h2 className="mb-1 text-xl font-normal">
+                      Author: {book.author}
+                    </h2>
+                    <p className="text-sm dark:text-gray-400">
+                      Genre: {book.genre}
+                    </p>
+                    <p className="text-sm dark:text-gray-400">
+                      Publication Date: {book.publication}
+                    </p>
+                  </div>
+                  <Button className="mt-10" asChild>
+                    <Link to="/book-details/:id">View Details</Link>
+                  </Button>
                 </div>
-                <div>
-                  <img
-                    src="https://source.unsplash.com/random/100x100/?5"
-                    alt=""
-                    className="object-cover w-full mb-4 h-60 sm:h-96 dark:bg-gray-500"
-                  />
-                  <h2 className="mb-1 text-xl font-normal">
-                    Author: Author Name
-                  </h2>
-                  <p className="text-sm dark:text-gray-400">
-                    Genre: Genre Name
-                  </p>
-                  <p className="text-sm dark:text-gray-400">
-                    Publication: Publication Date
-                  </p>
-                </div>
-                <Button className="mt-10" asChild>
-                  <Link to="/book-details/:id">View Details</Link>
-                </Button>
-              </div>
+              ))}
             </div>
 
             <Button className="mt-10" asChild>
---
+++ src/types/globalTypes.ts
@@ -0,0 +1,9 @@
+export interface IBook {
+  _id: number;
+  title: string;
+  author: string;
+  img: string;
+  genre: string;
+  publication: string;
+  comment: string[];
+}
